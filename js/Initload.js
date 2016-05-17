a21105ee62bc185a0a7066fadf738db50ddfdd4a
Jerry Simmons GISP <jerrysimmons@fourcty.org>
2016-05-17 11:08:11 -0400

    Add files via upload

--- js/Initload.js
+++ js/Initload.js
@@ -132,19 +132,23 @@ function initLoad() {
 function scan() {
     cordova.plugins.barcodeScanner.scan(
       function (result) {
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
+          //alert("We got a barcode\n" +
+          //      "Result: " + result.text + "\n" +
+          //      "Format: " + result.format + "\n" +
+          //      "Cancelled: " + result.cancelled);
+
+          $("#scanText").html("We got a barcode\n" + "Result: " + result.text + "\n" + "Format: " + result.format + "\n" + "Cancelled: " + result.cancelled);
+
       },
       function (error) {
-          alert("Scanning failed: " + error);
+          //alert("Scanning failed: " + error);
+          $("#scanText").html("Scanning failed: " + error);
       },
       {
           "preferFrontCamera": false, // iOS and Android
           "showFlipCameraButton": true, // iOS and Android
           "prompt": "Place a barcode inside the scan area", // supported on Android only
-          "formats": "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+          //"formats": "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
           "orientation": "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
       }
    );
